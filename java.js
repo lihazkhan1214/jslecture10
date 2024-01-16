2525bfb170619cd8a3e0078f37446aec1bb19db4
lihazkhan1214 <lehazali123@gmail.com>
2024-01-16 17:19:25 +0500

    add product

---
+++ java.js
@@ -0,0 +1,53 @@
+// console.log("welcome to javascript")
+// arrays 
+// const fruits=["apple","mangeo","banana"];
+
+// const storarry=fruits[0];
+// console.log(storarry)
+// console.log(fruits[1])
+// array methods
+
+// const fruits=["apple","mangeo",24,"banana"];
+// array lenght
+// const arraylenght=fruits.length;
+// console.log("original array",fruits)
+
+// const array=fruits.toString();
+
+// const array=fruits.at(0);
+// const array=fruits.join("#");
+
+// const array=fruits.pop();
+
+
+
+// console.log("after poping the array",fruits)
+
+// const array=fruits.push("orange");
+// console.log(fruits)
+
+// const array=fruits.unshift("grapes");
+// console.log(fruits)
+
+// console.log(array)
+// const myGirls = ["Cecilie", "Lone"];
+
+// const myBoys1 = ["joni", "alan", "das"];
+
+// const mychildren=myGirls.concat(myBoys,m[
+//     'Cecilie', 'Lone',
+//     'Emil',    'Tobias',
+//     'Linus',   'joni',
+//     'alan',    'das'
+//   ]yBoys1);
+const myBoys = ["Emil", "Tobias", "Linus"];
+// console.log(myBoys);
+
+// const array=myBoys.splice(0,1);
+
+// console.log(myBoys)
+
+const array=myBoys.slice(0,1);
+
+console.log(myBoys)
+console.log(array)
